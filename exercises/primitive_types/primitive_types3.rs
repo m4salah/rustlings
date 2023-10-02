9ba028221e3c22410ee9f9a8d2a43fab7313ce4f
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-10-03 01:21:01 +0300

    solve primitive types 3

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -12,6 +12,5 @@ fn main() {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
-        panic!("Array not big enough, more elements needed")
     }
 }
